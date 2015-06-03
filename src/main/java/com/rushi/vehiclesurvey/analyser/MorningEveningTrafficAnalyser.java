fc24ac487c859a3df6db007c669e4565b602846d
Rush <rushidadhaniya@gmail.com>
2015-06-03 19:12:24 +0530

    MorningEveningAnalyser modified to consider proper hours
    MorningEveningAnalyser modified to consider proper hours

--- src/main/java/com/rushi/vehiclesurvey/analyser/MorningEveningTrafficAnalyser.java
+++ src/main/java/com/rushi/vehiclesurvey/analyser/MorningEveningTrafficAnalyser.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.rushi.vehiclesurvey.vo.Date;
 import com.rushi.vehiclesurvey.vo.VehicleVO;
 
 public class MorningEveningTrafficAnalyser implements Analyser{
@@ -21,7 +22,7 @@ public class MorningEveningTrafficAnalyser implements Analyser{
 		
 		for(Map.Entry<Character, List<VehicleVO>> entry : vehicleDataMap.entrySet()) {
 			for(VehicleVO vehicle : entry.getValue()) {
-				if(vehicle.getStartDate().getHours() >= MORNING_STARTING_HOURS && vehicle.getStartDate().getHours() <= MORNING_END_HOURS) {
+				if(isVehicleInMorningSlot(vehicle.getStartDate(), vehicle.getEndDate())) {
 					morningVehicles.add(vehicle);
 				}
 				else {
@@ -33,6 +34,20 @@ public class MorningEveningTrafficAnalyser implements Analyser{
 		
 	}
 
+	private boolean isVehicleInMorningSlot(Date startDate, Date endDate) {
+		int hours = max(startDate.getHours(), endDate.getHours());
+		if(hours >= MORNING_STARTING_HOURS && hours <= MORNING_END_HOURS) {
+			return true;
+		}
+		return false;
+	}
+
+	private int max(int hours, int hours2) {
+		if(hours > hours2) {
+			return hours;
+		}
+		return hours2;
+	}
 	
 	
 }
