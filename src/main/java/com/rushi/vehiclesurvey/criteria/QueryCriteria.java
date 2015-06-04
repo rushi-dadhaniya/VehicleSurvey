dd95cf90a4b17e8a23da09010dd5ea824296005e
Rush <rushidadhaniya@gmail.com>
2015-06-04 18:47:03 +0530

    Files modified for test case
    Files modified for test case

--- src/main/java/com/rushi/vehiclesurvey/criteria/QueryCriteria.java
+++ src/main/java/com/rushi/vehiclesurvey/criteria/QueryCriteria.java
@@ -35,5 +35,49 @@ public class QueryCriteria {
 		return timeFrame + " " + analysisAttribute + " for " + direction + " direction/s";
 		
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((analysisAttribute == null) ? 0 : analysisAttribute
+						.hashCode());
+		result = prime * result
+				+ ((direction == null) ? 0 : direction.hashCode());
+		result = prime * result
+				+ ((timeFrame == null) ? 0 : timeFrame.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		QueryCriteria other = (QueryCriteria) obj;
+		if (analysisAttribute == null) {
+			if (other.analysisAttribute != null)
+				return false;
+		} else if (!analysisAttribute.equals(other.analysisAttribute))
+			return false;
+		if (direction == null) {
+			if (other.direction != null)
+				return false;
+		} else if (!direction.equals(other.direction))
+			return false;
+		if (timeFrame == null) {
+			if (other.timeFrame != null)
+				return false;
+		} else if (!timeFrame.equals(other.timeFrame))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
